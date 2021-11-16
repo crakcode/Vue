e86d6cac98f27982146959f7501308cb2e87e595
신현중 <estein92@forcs.com>
2021-11-16 17:13:31 +0900

    axios join/ login

--- src/components/Get.vue
+++ src/components/Get.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
-    <!-- <li v-for="(user,index) in users" :key="index"> -->
         {{users.id}}
         {{users.username}}
         {{users.password}}
-        ss
-    <!-- </li> -->
+    
+    <li v-for="(user,index) in user_list" :key="index">
+        {{user.id}}
+        {{user.username}}
+        {{user.password}}
+
+    </li>
 
         <!-- <img src="https://view01.wemep.co.kr/wmp-board/59/202010/20/x9d84mhzatle_faq.jpg"/> -->
     </div>
@@ -14,19 +18,34 @@
 import axios from 'axios';
 export default {
     name:'Axios',
+    
     data(){
+
         return{
-            users:[]
+            users:[],
+            user_list:[]
         }
     },
     created() {
       this.getData()
+      this.getUserList()
     },
     methods:{
         async getData(){
             const response = await axios.get('http://localhost:8080/userinfo')
+            const config = axios.create({
+                baseURL: process.env.VUE_APP_API_URL,
+                headers: {
+                Authorization: 'test1234',},})
+            console.log(config);
+
             this.users=response.data
             console.log(this.users)
+        },
+        async getUserList(){
+            const response = await axios.get('http://localhost:8080/userlist')
+            console.log(this.user_list)
+            this.user_list=response.data
         },
 
     }
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,13 +1,24 @@
 <template>
+<div>
         <div>
 id,email,name,password
-            <input v-model="id" placeholder="아이디"/> 
             <input v-model="email" placeholder="이메일"/> 
             <input v-model="name" placeholder="이름"/> 
             <input v-model="password" placeholder="비밀번호"/> 
-            <button @click="write">회원가입</button>
+            <button @click="register">회원가입</button>
             {{id}}
         </div>
+        <div>
+            <input v-model="email" placeholder="이메일"/> 
+            <input v-model="password" placeholder="비밀번호"/> 
+            <button @click="login">회원가입</button>
+        </div>
+        <div>
+            <input v-model="token" placeholder="토큰"/> 
+            <button @click="authSource">토큰 보내기</button>
+
+        </div>
+</div>
 </template>
 
 <script>
@@ -20,18 +31,49 @@ export default {
             id:this.id,
             email:"",
             name:"",
-            password:""
+            password:"",
+            token:"",
         }
     },
 
     methods:{
-       write(){
-            axios.post('/join/',{ 
-            id:this.id,
+       register(){
+            axios.post('http://localhost:8080/join/',{ 
                email:this.email,
                name:this.name,
                password:this.password, })
-       } 
+       },
+        async login(){
+            const instance = axios.create({
+                baseURL: 'http://localhost:3000/',
+                  headers: {
+                Authorization: 'test1234', // header의 속성
+                },
+            });
+
+            const response=await instance.post('http://localhost:8080/login/',{ 
+               email:this.email,
+               name:this.name,
+               password:this.password, })
+            const hello= response.status==200?response.data:"error";
+            console.log(response)
+            console.log(hello)
+       },
+
+        async authSource(){
+            const instance = axios.create({
+                baseURL: 'http://localhost:8080/',
+                  headers: {
+                "X-AUTH-TOKEN":this.token
+                },
+            });
+            const response=await instance.post('http://localhost:8080/user/api')
+            // const hello= response.status==200?response.data:"error";
+            console.log(response)
+            // console.log(hello)
+       },
+
+
 
     }
 
